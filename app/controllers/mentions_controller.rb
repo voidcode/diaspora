a24e8249a8e7f4591d937869ab7397d24129aaaa
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-08-03 10:05:01 -0700

    fix mentions query

--- app/controllers/mentions_controller.rb
+++ app/controllers/mentions_controller.rb
@@ -6,15 +6,11 @@ class MentionsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    puts params.inspect
     @aspect_ids = current_user.aspect_ids
     @aspect = :all
-    mentions =  Mention.where(:person_id => current_user.person.id).limit(15).order('created_at desc')
-    mentions = mentions.where('created_at <= ?', Time.at(params[:max_time].to_i)) if params[:max_time].present?
-    mentions = mentions.includes(:post => [{:author => :profile}, :photos, :mentions])
-  
-    posts = mentions.map{|m| m.post}
-    pp posts
+    posts = Post.joins(:mentions).where(:mentions => {:person_id => current_user.person.id}).order('posts.created_at desc').includes({:author => :profile}, :comments).limit(15)
+    posts = posts.where('posts.created_at < ?', Time.at(params[:max_time].to_i)) if params[:max_time].present?
+
     @selected_people = posts.map{|p| p.author}.uniq
     @selected_people_count = @selected_people.count
     @posts = PostsFake.new(posts)
